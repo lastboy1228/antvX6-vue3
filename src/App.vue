901591f512526abaf1f433507833eca4aabe0b99
lastboy1228 <lastboy1228@gmail.com>
2024-05-11 18:08:54 +0800

    不检查未知数据类型

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="app-content">
-    <div id="container" style="position: relative; width: 100%; height: 100%"></div>
-    <div id="overlay" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; pointer-events: none"></div>
-
-    <!-- 自定义component被渲染时，在逻辑关系上都位于这个Container内 -->
-    <!-- 然后被teleport到对应节点的foreignObject内 -->
-    <TeleportContainer />
-  </div>
+    <div class="app-content">
+        <div id="container"></div>
+        <!-- 自定义component被渲染时，在逻辑关系上都位于这个Container内 -->
+        <!-- 然后被teleport到对应节点的foreignObject内 -->
+        <TeleportContainer />
+    </div>
 </template>
 
 <script lang="ts">
@@ -20,60 +18,110 @@ import {getTeleport, register} from "@antv/x6-vue-shape";
 const TeleportContainer = getTeleport();
 
 register({
-  shape: "custom-progress",
-  component: ProgressNode
+    shape: "custom-progress",
+    component: ProgressNode
 });
 
 export default defineComponent({
-  name: "App",
-  components: {
-    TeleportContainer
-  },
-  mounted() {
-    const graph = new Graph({
-      container: document.getElementById("container")!,
-      background: {
-        color: "#F2F7FA"
-      },
-      autoResize: true
-    });
-    window.__x6_instances__ = [];
-    window.__x6_instances__.push(graph);
+    name: "App",
+    components: {
+        TeleportContainer
+    },
+    mounted() {
+        const graph = new Graph({
+            container: document.getElementById("container")!,
+            background: {
+                color: "#F2F7FA"
+            },
+            autoResize: true
+        });
+        window.__x6_instances__ = [];
+        window.__x6_instances__.push(graph);
 
-    const node = graph.addNode({
-      shape: "custom-progress",
-      x: 60,
-      y: 60,
-      width: 100,
-      height: 100,
-      data: {
-        progress: 10
-      }
-    });
+        // 添加分割线
+        graph.addEdge({
+            source: {x: 0, y: graph.options.height / 2},
+            target: {x: graph.options.width, y: graph.options.height / 2},
+            attrs: {
+                line: {
+                    stroke: "#333",
+                    strokeWidth: 2,
+                    targetMarker: null,
+                    strokeDasharray: "5,5"
+                }
+            },
+            data: {
+                splitter: true,
+                horizontal: true
+            }
+        });
+        graph.addEdge({
+            source: {x: graph.options.width / 2, y: 0},
+            target: {x: graph.options.width / 2, y: graph.options.height},
+            attrs: {
+                line: {
+                    stroke: "#333",
+                    strokeWidth: 2,
+                    targetMarker: null,
+                    strokeDasharray: "5,5"
+                }
+            },
+            data: {
+                splitter: true,
+                horizontal: false
+            }
+        });
+        // 监听连线移动
+        graph.on("edge:moved", ({x, y, edge}) => {
+            if (edge.getData().splitter) {
+                if (edge.getData().horizontal) {
+                    edge.prop("source", {x: 0, y: y});
+                    edge.prop("target", {x: graph.options.width, y: y});
+                } else {
+                    edge.prop("source", {x: x, y: 0});
+                    edge.prop("target", {x: x, y: graph.options.height});
+                }
+            }
+        });
 
-    // 自动更新进度条
-    setInterval(() => {
-      const {progress} = node.getData();
-      var newProgress;
-      if (progress >= 100) {
-        newProgress = 0;
-      } else if (progress > 90) {
-        newProgress = (progress + 10) % 100;
-      } else {
-        newProgress = progress + 10;
-      }
-      node.setData({
-        progress: newProgress
-      });
-    }, 1000);
-  }
+        const node = graph.addNode({
+            shape: "custom-progress",
+            x: 60,
+            y: 60,
+            width: 100,
+            height: 100,
+            data: {
+                progress: 10
+            }
+        });
+
+        // 自动更新进度条
+        setInterval(() => {
+            const {progress} = node.getData();
+            var newProgress;
+            if (progress >= 100) {
+                newProgress = 0;
+            } else if (progress > 90) {
+                newProgress = (progress + 10) % 100;
+            } else {
+                newProgress = progress + 10;
+            }
+            node.setData({
+                progress: newProgress
+            });
+        }, 1000);
+    }
 });
 </script>
 <style scoped>
 .app-content {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 98vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 98vh;
+}
+#container {
+    width: 100%;
+    height: 100%;
 }
 </style>
